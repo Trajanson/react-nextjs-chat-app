fa8f2beb63ade9843d20cf4cbde4c36817af5b2e
Nick Cook <nick@nickwcook.com>
2018-11-23 22:33:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
-
 const cors = require('cors');
 const next = require('next');
 const Pusher = require('pusher');
@@ -8,11 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const Sentiment = require('sentiment');
-
-const firebaseApp = firebase.initializeApp(
-  functions.config().firebase
-)
-
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 
@@ -21,61 +13,67 @@ const handler = app.getRequestHandler();
 const sentiment = new Sentiment();
 
 const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_APP_SECRET,
-  cluster: process.env.PUSHER_APP_CLUSTER,
-  encrypted: true
-});
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://<YOUR-APP-NAME>.herokuapp.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+      appId: process.env.PUSHER_APP_ID,
+      key: process.env.PUSHER_APP_KEY,
+      secret: process.env.PUSHER_APP_SECRET,
+      cluster: process.env.PUSHER_APP_CLUSTER,
+      encrypted: true
 })
 
 app.prepare()
-  .then(() => {
+      .then(_ => {
 
-    const server = express();
+            const server = express();
 
-    server.use(cors());
-    server.use(bodyParser.json());
-    server.use(bodyParser.urlencoded({ extended: true }));
+            server.use(cors());
+            server.use(bodyParser.json());
+            server.use(bodyParser.urlencoded({ extended: true }));
 
-    server.get('*', (req, res) => {
-      return handler(req, res);
-    })
+            server.get('*', (req, res) => {
+                  return handler(req, res);
+            })
 
-const chatHistory = { messages: [] };
+            const chatHistory = {
+                  messages: []
+            }
 
-server.post('/message', (req, res, next) => {
-  const { user = null, message = '', timestamp = +new Date } = req.body;
-  const sentimentScore = sentiment.analyze(message).score;
+            server.post('/message', (req, res, next) => {
+                  const { user = null, message = '', timestamp = +new Date } = req.body;
+                  const sentimentScore = sentiment.analyze(message).score;
 
-  const chat = { user, message, timestamp, sentiment: sentimentScore };
+                  const chat = {
+                        user,
+                        message,
+                        timestamp,
+                        sentiment: sentimentScore
+                  }
 
-  chatHistory.messages.push(chat);
-  pusher.trigger('chat-room', 'new-message', { chat });
+                  chatHistory.messages.push(chat);
+                  pusher.trigger('chat-room', 'new-message', { chat });
 
-  res.send(Widgets.create());
-});
+                  res.send(Widgets.create());
+            })
 
-server.post('/messages', (req, res, next) => {
-  res.json({ ...chatHistory, status: 'success' });
+            server.post('/messages', (req, res, next) => {
+                  res.json({
+                        ...chatHistory,
+                        status: 'success'
+                  })
 
-  res.send(Widgets.create());
-});
+                  res.send(Widgets.create());
+            })
 
-server.listen(port, err => {
-  if (err) throw err;
-  console.log(`> Ready on http://localhost:${port}`);
-});
+            server.listen(port, err => {
+                  if (err) {
+                        throw err;
+                  }
+                  console.log(`> Ready on http://localhost:${port}`);
+            })
 
-})
-.catch(ex => {
-console.error(ex.stack);
-process.exit(1);
-});
+      })
+      .catch(ex => {
+            console.error(ex.stack);
+            process.exit(1);
+      })
 
 exports.widgets = functions.https.onRequest(server);
